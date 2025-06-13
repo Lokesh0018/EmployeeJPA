30f8ee929611fbf76ae9c81ca7b78c003e9ff73a
Lokesh0018 <munakalalokesh222@gmail.com>
2025-06-13 14:45:09 +0530

    final

--- src/main/java/com/employee/employee_jpa/controller/EmployeeController.java
+++ src/main/java/com/employee/employee_jpa/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import com.employee.employee_jpa.entity.Employee;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -54,5 +55,14 @@ public class EmployeeController {
         emp.get().setSalary(entity.getSalary());
         return new ResponseEntity<>(er.save(emp.get()),HttpStatus.OK);
     }   
+
+    @DeleteMapping("/api/employee/{id}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable long id){
+        Optional<Employee> emp = er.findById(id);
+        if(!emp.isPresent())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        er.delete(emp.get());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
     
 }
