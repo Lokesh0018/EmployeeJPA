b88bf75e269bef51d712801eab369f16ebdbe419
Lokesh0018 <munakalalokesh222@gmail.com>
2025-06-14 13:08:17 +0530

    first

--- src/main/java/com/employee/employee_jpa/controller/EmployeeController.java
+++ src/main/java/com/employee/employee_jpa/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import com.employee.employee_jpa.entity.Employee;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class EmployeeController {
     @Autowired
